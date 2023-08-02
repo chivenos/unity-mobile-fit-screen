8cad43fe0eabcb3c5f9e963424b563e4cfdff80c
chivenos <64312747+chivenos@users.noreply.github.com>
2023-08-02 13:31:15 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControllerScript : MonoBehaviour
+public class CameraScript : MonoBehaviour
 {
+    //the code will align the bottom of the camera as the bottom boundry position and the height of it will change with the device screen type.
+    //place boundaries into the game scene
+    [SerializeField] private GameObject LeftBoundry; 
+    [SerializeField] private GameObject RightBoundry;
+    [SerializeField] private GameObject BottomBoundry;
+
     // Start is called before the first frame update
     void Start()
     {
-       ScaleScreenSize(Screen.safeArea.height,Screen.safeArea.width);
+        ScaleScreenSize();
     }
-    
-    private void ScaleScreenSize(float x, float y)
+
+    private void ScaleScreenSize()
     {
-        float ratio = x / y;
-        float size = Camera.main.orthographicSize * ratio;
-        Camera.main.orthographicSize = size;
+        float halfWidth = (RightBoundry.transform.position.x - LeftBoundry.transform.position.x)/2;
+
+        Camera.main.orthographicSize = halfWidth / Camera.main.aspect;
+        Vector3 camPos = Camera.main.transform.position;
+        Camera.main.transform.position = new Vector3(camPos.x, Camera.main.orthographicSize + BottomBoundry.transform.position.y, camPos.z);
     }
 }
